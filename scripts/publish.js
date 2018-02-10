27eb4aff1c3e3b4180a48d73ac684e93b68f06a3
Kevin Smith <KevinH.Smith@coxinc.com>
2018-02-10 00:04:48 -0500

    Add publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,3 +1,13 @@
 const $ = require('./exec');
 
 require('./build.js');
+
+$('git checkout gh-pages');
+
+try {
+  $('cp dist/index.html ./');
+  $('git commit -a -m "Update gh-pages"');
+  $('git push');
+} finally {
+  $('git checkout master');
+}
